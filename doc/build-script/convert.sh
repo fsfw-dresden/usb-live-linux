ca0a709b55c42c50cef27ab6249c6a5673dee7ae
cknoll <CarstenKnoll@gmx.de>
2016-10-04 20:28:42 +0200

    das convert-script kann jetzt alle md-Dateien auf einmal nach html konvertieren

--- doc/build-script/convert.sh
+++ doc/build-script/convert.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-pandoc --standalone --template fsfw-template.html content.md -o output.html
+
+
+# convert a single file 
+#pandoc --standalone --template fsfw-template.html content.md -o output.html
+
+
+# convert all markdown files at once
+
+ORIGPATH=`pwd`
+
+cd ../src/
+
+for f in *.md ;
+do
+cmd="pandoc --standalone --template $ORIGPATH/fsfw-template.html ${f} -o ../html/${f%%.md}.html"
+
+# for debugging:
+# echo $cmd
+eval $cmd
+done
+
+
+cd $ORIGPATH
