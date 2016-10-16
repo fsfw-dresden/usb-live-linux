45245d1d548cfc0f5a389614d20086d1f95d2549
cknoll <CarstenKnoll@gmx.de>
2016-10-16 22:12:30 +0200

    autostart skript angepasst

--- FSFW-Uni-Stick/config/includes.chroot/home/user/aux-files/home-dir/.kde/Autostart/fsfw-autostart-srcipt.sh
+++ FSFW-Uni-Stick/config/includes.chroot/home/user/aux-files/home-dir/.kde/Autostart/fsfw-autostart-srcipt.sh
@@ -3,12 +3,12 @@
 # Dieses Skript wird beim Start von KDE automatisch ausgeführt.
 
 
-# Unsere Startseite laden
-nohup firefox /home/user/FSFW-Material/stick-doku/index.html
-
 
 sleep 2
 
 # das dolphin-Fenster schließen, durch eine ungünstige Konfiguration am Anfang aufgeht
 killall dolphin
 
+
+# Unsere Startseite laden
+nohup firefox /home/user/FSFW-Material/stick-doku/index.html
