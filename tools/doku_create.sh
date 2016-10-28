fdbb6f4bb1693b69391547cb1d27e9ed6737e040
gerdg-dd <gerdg-dd@gmx.de>
2016-10-28 10:27:51 +0200

    tools/fsfw-user_config.sh - angepasst

--- tools/doku_create.sh
+++ tools/doku_create.sh
@@ -7,42 +7,9 @@
 echo " "
 echo "Doku bauen und verteilen"
 
-#### erstellt von Carsten -->
+# Hier allgemeine Doku zu Progammen oder Funkionen des Live Systems
+# FSFW Material / Doku --> fsfw-user_config.sh
 
-cd ../doc/src/
-
-for f in *.md ;
-    do
-
-    TARGETFILE="../html/${f%%.md}.html"
-
-# TODO: Fehler! fals Paket pandoc nicht installiert ist -- Programmverfügbarkeit vorher testen
-    cmd="pandoc --standalone --template $ORIGPATH/../doc/build-script/fsfw-template.html ${f} -o $TARGETFILE"
-
-    # for debugging:
-    # echo $cmd
-    eval $cmd
-
-    # in the markdown docs there are markdown link targets (to play nicely with github)
-    # now its time to convert them
-
-    perl -p -i -e 's/\.md">/\.html">/g' $TARGETFILE
-    
-    echo "Datei geschrieben:" $TARGETFILE
-done
-
-## erstellt Carsten <--
-
-cd ${OLDPWD}
-
-# TODO: *.hlml  --> ../../FSFW-Uni-Stick/config/includes.chroot/var/www/
-
-if [ ! -d config/includes.chroot/home/user/FSFW-Material/stick-doku/ ]; then
-	 mkdir -p config/includes.chroot/home/user/FSFW-Material/stick-doku/
-	 echo " config/includes.chroot/home/user/FSFW-Material/stick-doku/ erstellt"
-fi 
-
-rsync -avP --delete ../doc/html config/includes.chroot/home/user/FSFW-Material/stick-doku
 
 echo "Doku-Erstellung und Verteilung fertig."
 
--- tools/fsfw-user_config.sh
+++ tools/fsfw-user_config.sh
@@ -14,3 +14,48 @@ echo " FSFW_UNI_STICK_VERSION = "$(echo "$(../tools/calc-version-number.sh)")" "
 echo " git-revision = https://github.com/fsfw-dresden/usb-live-linux/tree/$(git rev-parse master)" >> config/includes.chroot/home/user/.version_fsfw-uni-stick
 
 #
+
+echo "FSFW Material/Doku bauen und verteilen"
+
+# allgemeine Doku zu Progammen oder Funkionen des Live Systems --> doku_create.sh
+
+#### erstellt von Carsten / angepasst gerd -->
+
+dlist_md=(../doc/src/*.md)
+
+for f in ${dlist_md[@]##*/} ; 
+   do
+    TARGETFILE="../doc/html/${f%%.md}.html"
+
+# TODO: Fehler! fals Paket pandoc nicht installiert ist -- Programmverfügbarkeit vorher testen
+    cmd="pandoc --standalone --template ../doc/build-script/fsfw-template.html ${dlist_md} -o $TARGETFILE"
+
+    # for debugging:
+    # echo $cmd
+    eval $cmd
+
+    # in the markdown docs there are markdown link targets (to play nicely with github)
+    # now its time to convert them
+
+    perl -p -i -e 's/\.md">/\.html">/g' $TARGETFILE
+    
+    echo "Datei geschrieben:" $TARGETFILE
+done
+
+## erstellt Carsten <--
+
+
+# TODO: *.hlml  --> ../../FSFW-Uni-Stick/config/includes.chroot/var/www/
+
+if [ ! -d config/includes.chroot/home/user/FSFW-Material/stick-doku/ ]; then
+	 mkdir -p config/includes.chroot/home/user/FSFW-Material/stick-doku/
+	 echo " config/includes.chroot/home/user/FSFW-Material/stick-doku/ erstellt"
+fi 
+
+rsync -avP --delete ../doc/html/ config/includes.chroot/home/user/FSFW-Material/stick-doku
+
+# doc/latex-vorlage  übernehmen
+
+rsync -avP --delete ../doc/latex-vorlage config/includes.chroot/home/user/FSFW-Material
+
+echo "FSFW Doku-Erstellung und Verteilung fertig."
