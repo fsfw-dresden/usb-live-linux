dc5f87936a4391c2d0075f55b9061d6c01724ad7
cknoll <CarstenKnoll@gmx.de>
2017-02-06 17:05:08 +0100

    jupyter start-skript angepasst um Token-Eingabe zu unterdrücken (Benutzbarkeit an dieser Stelle höher gewichtet als Sicherheit)

--- doc/src_fsfw-user_config/bin/jupyter_start_script.sh
+++ doc/src_fsfw-user_config/bin/jupyter_start_script.sh
@@ -3,6 +3,11 @@
 # This script starts the jupyter server and then opens firefox at the respective URL.
 # Normally this should happen automatically but this does not work reliably.
 
+# To reduce the barrier for new users to see an acutal usefull notebook, we deactivate
+# the security token. This is OK for a demo on a live system but not recommended on a
+# longer lasting system. See commands below and [1] for background information.
+# [1] http://blog.jupyter.org/2016/12/21/jupyter-notebook-4-3-1/
+
 
 
 function start_firefox() {
@@ -21,4 +26,12 @@ firefox http://localhost:8888/
 
 start_firefox &
 
-jupyter notebook 
+# demo version without security token (reduces user barrier)
+jupyter notebook --NotebookApp.token=""
+
+
+# more secure version of the starter command
+# (recommended  for productive usage, may require to copy
+# and paste some security token string):
+
+# jupyter notebook
