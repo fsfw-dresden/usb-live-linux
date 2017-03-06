88ced65965e4c728bb4bda0ab09f2233011d3728
Nos- <Nos-@users.noreply.github.com>
2017-03-06 08:46:39 +0100

    Workaround für Issue #53 (Firefox-Plugins (Literaturverwaltung & Co.))

--- doc/src_fsfw-user_config/.kde/Autostart/fsfw-autostart-skript.sh
+++ doc/src_fsfw-user_config/.kde/Autostart/fsfw-autostart-skript.sh
@@ -7,10 +7,15 @@
 #sleep 2
 
 
-# das dolphin-Fenster schließen, durch eine ungünstige Konfiguration am Anfang aufgeht
+# das dolphin-Fenster schließen, das durch eine ungünstige Konfiguration am Anfang aufgeht
 # (Das ist inzwischen Überflüssig und kann demnächst gelöscht werden)
 #killall dolphin
 
 
 # Unsere Startseite laden
+nohup firefox /home/user/FSFW-Material/stick-doku/index.html
+
+# Beim ersten Start erscheint nun zusätzlich im Webbrowser eine Begrüßungsseite des Zotero-Plugins...
+#...um unsere Startseite wieder in den Vordergrund zu bringen, folgender Workaround.
+sleep 5
 nohup firefox /home/user/FSFW-Material/stick-doku/index.html
