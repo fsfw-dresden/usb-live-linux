679962a3cb9ea7de88de589c0e73a3024c7f1c1c
Carsten Knoll <carsten.knoll@posteo.de>
2017-10-15 22:15:35 +0200

    Entferne überflüssige Leerzeichen aus extra-install_paket.sh

--- tools/extra-install_paket.sh
+++ tools/extra-install_paket.sh
@@ -2,11 +2,11 @@
 #
 # Skript erstellt gerdg-dd@gmx.de 2016-10-21
 #
-# Paketlisten nach extra-install Paketen durchsuchen und download nach config/packages.chroot/* 
+# Paketlisten nach extra-install Paketen durchsuchen und download nach config/packages.chroot/*
 #
 
 FSFW_UNI_STICK_CONFIG=$1
-echo "extra-install_paket.sh  FSFW-Uni-Stick config: ${FSFW_UNI_STICK_CONFIG} " 
+echo "extra-install_paket.sh  FSFW-Uni-Stick config: ${FSFW_UNI_STICK_CONFIG} "
 
 DOWNLOAD="wget -nv -T10 --no-http-keep-alive --show-progress -c"
 PAKET_LISTEN=($(ls ./config/package-lists/*))
@@ -17,14 +17,14 @@ echo "Extra Paket-Installation für packages.chroot vorbereiten."
 
 for paket_liste in ${PAKET_LISTEN[@]}
   do
-	while read line 
-	do 
+	while read line
+	do
 	    case "${line%%"  "*}" in
 	    "## --extra--installation -->")
 		# echo "download = ${line##*-->}"
 		paket_quelle=${line##*-->}
 		paket=${paket_quelle##*/}
-				
+
 		if [ -n "${paket}" ]; then
 			# echo "download = ${paket}"
 			if [ -e ../config/${FSFW_UNI_STICK_CONFIG}/system_config/packages.chroot/${paket} ];
@@ -34,7 +34,7 @@ for paket_liste in ${PAKET_LISTEN[@]}
 				if [ ! -d ../config/${FSFW_UNI_STICK_CONFIG}/system_config/packages.chroot/ ]; then
 				 mkdir -p ../config/${FSFW_UNI_STICK_CONFIG}/system_config/packages.chroot/
 				 echo " ../config/${FSFW_UNI_STICK_CONFIG}/system_config/packages.chroot/ erstellt"
-				fi 
+				fi
 				${DOWNLOAD} ${paket_quelle} -O ../config/${FSFW_UNI_STICK_CONFIG}/system_config/packages.chroot/${paket_quelle##*/}
 				echo "${paket_quelle##*/} - geholt "
 			fi
@@ -43,10 +43,10 @@ for paket_liste in ${PAKET_LISTEN[@]}
 	    ;;
 	    *)
 		#       echo " Unbekannt "
-	    ;;  
-	
+	    ;;
+
 	    esac
-	
+
 	done < ${paket_liste}
 
 done
@@ -57,4 +57,3 @@ echo " extra-install_paket.sh --system_config  aktuallisiert fertig."
 
 
 echo "Fertig - Extra Paket-Installation für packages.chroot vorbereiten."
-
