5def37e454b640a33cbd9392487f4dad03e6f26d
Marcel Partap <mpartap@gmx.net>
2018-08-30 18:13:04 +0200

    scripts/apply-build-profile.sh: Don't ask.

--- scripts/apply-build-profile.sh
+++ scripts/apply-build-profile.sh
@@ -7,5 +7,5 @@ cd_repo_root
 FSFW_UNI_STICK_CONFIG=$1
 echo "FSFW-Uni-Stick ${0} ${FSFW_UNI_STICK_CONFIG} " 
 
-ln -svi profiles/${FSFW_UNI_STICK_CONFIG}/auto/config auto/config
+ln -sv profiles/${FSFW_UNI_STICK_CONFIG}/auto/config auto/config
 rsync -avP profiles/${FSFW_UNI_STICK_CONFIG}/system_config/ config/
