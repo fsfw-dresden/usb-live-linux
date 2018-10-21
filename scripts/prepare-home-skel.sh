0a4ca5456e9153ea8da34ffd7b20f1341a436a18
Marcel Partap <mpartap@gmx.net>
2018-10-21 14:08:07 +0200

    scripts/prepare-home-skel.sh: bischen aufgeräumt.

--- scripts/prepare-home-skel.sh
+++ scripts/prepare-home-skel.sh
@@ -7,16 +7,6 @@ cd_repo_root
 BUILD_VARIANT=$(readlink variants/active)
 echo "Live-Stick ${0} ${BUILD_VARIANT}" 
 
-# === FIXME KILL => auto/clean
-# aufräumen ( ist ../home/user vorhanden wird die config nicht aus ../etc/skel übernommen)
-  if [ -d config/includes.chroot/home/ ]; then
-         echo "etwas ist sehr fail"
-         read -n1
-	 rm -Ri config/includes.chroot/home
-	 echo " config/includes.chroot/home gelöscht"
-  fi 
-# === FIXME KILL => auto/clean
-
 mkdir -pv config/includes.chroot/etc/skel
 rsync --verbose --archive --stats --info=progress2 variants/${BUILD_VARIANT}/user-config/ config/includes.chroot/etc/skel/
 scripts/copy-docs.sh
@@ -27,4 +17,4 @@ echo "schreibe git-versionsnummer & URL in HOME/.version_fsfw-uni-stick"
 echo "FSFW_UNI_STICK_VERSION = $(scripts/calc-version-number.sh)" > config/includes.chroot/etc/skel/.version_fsfw-uni-stick
 echo "git-revision = https://github.com/fsfw-dresden/usb-live-linux/tree/$(git rev-parse master)" >> config/includes.chroot/etc/skel/.version_fsfw-uni-stick
 
-echo "fsfw-uni-stick_user_config.sh  beendet"
+echo "${0} beendet"
