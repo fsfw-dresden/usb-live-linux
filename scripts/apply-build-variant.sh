f41ef46712027ac39fe72ae67b798041c3b521c1
Marcel Partap <mpartap@gmx.net>
2018-10-21 14:16:26 +0200

    scripts/apply-build-variant.sh: auto-Verzeichnispfad auch geändert..

--- scripts/apply-build-variant.sh
+++ scripts/apply-build-variant.sh
@@ -7,5 +7,5 @@ cd_repo_root
 BUILD_VARIANT=$(readlink variants/active)
 echo "Live-Stick ${0} ${BUILD_VARIANT}" 
 
-ln -svf ../variants/${BUILD_VARIANT}/auto/config auto/config
+ln -svf ../variants/${BUILD_VARIANT}/system-config/auto/config auto/config
 rsync -avP variants/${BUILD_VARIANT}/system-config/ config/
