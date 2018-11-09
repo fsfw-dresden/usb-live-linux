a7d843edf3a0ffa16722ef42de08c79f425517e3
Marcel Partap <mpartap@gmx.net>
2018-11-09 23:55:23 +0100

    Xfce_stretch: fix stick-doku firefox autostart

--- variants/FSFW-Uni-Stick_Xfce_stretch_amd64/user-config/.config/autostart-scripts/fsfw-autostart-skript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Dieses Skript wird beim Start von KDE automatisch ausgeführt.
-sleep 2
-
-# Unsere Startseite laden
-pgrep firefox || nohup firefox ~/FSFW-Material/stick-doku/index.html
