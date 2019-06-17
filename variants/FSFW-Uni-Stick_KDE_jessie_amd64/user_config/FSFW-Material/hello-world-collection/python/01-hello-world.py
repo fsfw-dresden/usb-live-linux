300728df1b229dc0d2798de5bc243411fdc19efb
gerdg-dd <gerdg-dd@gmx.de>
2019-06-17 21:33:31 +0200

    alte variante löschen - FSFW-Uni-Stick_KDE_jessie_amd64

--- variants/FSFW-Uni-Stick_KDE_jessie_amd64/user_config/FSFW-Material/hello-world-collection/python/01-hello-world.py
+++
@@ -1,3 +0,0 @@
-# This is a very simple program. You can run it from the command line via `python3 01-hello-world.py`.
-
-print("Hello World.")
--- variants/FSFW-Uni-Stick_KDE_jessie_amd64/user_config/FSFW-Material/hello-world-collection/python/05-generate-docs/fsfw_demo_package/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file causes python to recognize the whole directory as a package.
-# It must either be empty or contain valid python code.
-# This code is executed when the package is imported.
