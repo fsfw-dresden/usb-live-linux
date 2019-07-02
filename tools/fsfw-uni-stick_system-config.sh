ba7163491ee949dd7a3612c6baabd025bd6ee844
gerdg-dd <gerdg-dd@gmx.de>
2019-07-02 15:10:36 +0200

    bereinigen von  tools/fsfw-uni-stick_system-config.sh - build Verzeichnis aufräumen übernimmt FSFW-Uni-Stick/auto/clean

--- tools/fsfw-uni-stick_system-config.sh
+++ tools/fsfw-uni-stick_system-config.sh
@@ -17,41 +17,6 @@ echo "FSFW-Uni-Stick System config: variant PATH = ${VARIANT_PATH}  -- variant =
 rm ./auto/config
 ln -s ${VARIANT_PATH}/${FSFW_UNI_STICK_VARIANT}/config ./auto/config
 
-# aufräumen 
-# - ist config/includes.chroot/*  vorhanden --> löschen
-
- if [ -d config/includes.chroot/ ]; then
-  rm -R config/includes.chroot/*
-  echo " löschen - config/includes.chroot/* "
- fi
-
- if [ -d config/apt/ ]; then
-  rm -R config/apt/*
-  echo " löschen - config/apt/* "
- fi
-
-
- if [ -d config/packages.chroot/ ]; then
-  rm -R config/packages.chroot/*
-  echo " löschen - config/packages.chroot/* "
- fi
-
- if [ -d config/archives/ ]; then
-  rm -R config/archives/*
-  echo " löschen - config/archives/* "
- fi
-
- if [ -d config/bootloaders/ ]; then
-  rm -R config/bootloaders/*
-  echo " löschen - config/bootloaders/* "
- fi
-
- if [ -d config/templates/ ]; then
-  rm -R config/templates/*
-  echo " löschen - config/templates/* "
- fi
-
-
 # system_config übernehmen
 
 echo " system_config ${FSFW_UNI_STICK_VARIANT} schreiben "
