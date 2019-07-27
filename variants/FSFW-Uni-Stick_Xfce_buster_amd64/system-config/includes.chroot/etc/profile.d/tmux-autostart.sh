b7c77e4b63331c4cb638ef544ecb4a7b86c49761
Marcel Partap <mpartap@gmx.net>
2019-07-27 17:07:01 +0200

    Xfce_buster: fix up root shell selection for live system

--- variants/FSFW-Uni-Stick_Xfce_buster_amd64/system-config/includes.chroot/etc/profile.d/tmux-autostart.sh
+++ variants/FSFW-Uni-Stick_Xfce_buster_amd64/system-config/includes.chroot/etc/profile.d/tmux-autostart.sh
@@ -3,7 +3,7 @@
 
 if read -sn1 -t2 -sp "$(color bold yellow red)Within 2 seconds, press [s] for a root shell or [any] other key to enter tmux$(color off)"; then
   case $REPLY in
-    s) echo; su;;
+    s) echo; sudo su -l;;
     *) echo; tmux -2 attach-session;;
   esac
 fi
