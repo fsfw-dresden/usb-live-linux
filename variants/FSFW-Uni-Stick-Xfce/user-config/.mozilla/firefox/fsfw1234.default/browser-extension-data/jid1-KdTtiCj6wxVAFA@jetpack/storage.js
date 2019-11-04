ae38bab6343ae10cacdcbc7d453b44f522d6cc66
Marcel Partap <mpartap@gmx.net>
2019-11-04 15:31:09 +0100

    FSFW-Uni-Stick: provide useful search engines for students

---
+++ variants/FSFW-Uni-Stick-Xfce/user-config/.mozilla/firefox/fsfw1234.default/browser-extension-data/jid1-KdTtiCj6wxVAFA@jetpack/storage.js
@@ -0,0 +1,284 @@
+{
+  "allowPopupOnEditableFields": true,
+  "autoCopyToClipboard": "off",
+  "contextMenuItemBehaviour": "new-bg-tab",
+  "contextMenuString": "Suche nach “%s”",
+  "enableEnginesInContextMenu": true,
+  "hidePopupOnPageScroll": false,
+  "hidePopupOnRightClick": true,
+  "hidePopupOnSearch": false,
+  "iconAlignmentInGrid": "left",
+  "maxSelectedCharacters": 0,
+  "middleMouseSelectionClickMargin": 14,
+  "minSelectedCharacters": 0,
+  "mouseLeftButtonBehaviour": "new-bg-tab",
+  "mouseMiddleButtonBehaviour": "new-bg-tab",
+  "nPopupIconsPerRow": 9,
+  "popupAnimationDuration": 100,
+  "popupBackgroundColor": "#FFFFFF",
+  "popupBorderRadius": 2,
+  "popupDelay": 100,
+  "popupDisableCommand": "Ctrl+Shift+U",
+  "popupHighlightColor": "#3399FF",
+  "popupItemBorderRadius": 0,
+  "popupItemHoverBehaviour": "highlight",
+  "popupItemPadding": 7,
+  "popupItemSize": 32,
+  "popupItemVerticalPadding": 5,
+  "popupLocation": "selection",
+  "popupOffsetX": 0,
+  "popupOffsetY": 0,
+  "popupOpenBehaviour": "auto",
+  "popupOpenCommand": "Ctrl+Space",
+  "popupPaddingX": 3,
+  "popupPaddingY": 1,
+  "popupSeparatorWidth": 60,
+  "searchEngines": [
+    {
+      "type": "sss",
+      "id": "copyToClipboard",
+      "isPlainText": false,
+      "isEnabled": false,
+      "isEnabledInContextMenu": false
+    },
+    {
+      "type": "sss",
+      "id": "openAsLink",
+      "isEnabled": false,
+      "isEnabledInContextMenu": false
+    },
+    {
+      "type": "browser",
+      "name": "Ecosia",
+      "iconUrl": "https://api.faviconkit.com/www.ecosia.org/64",
+      "searchUrl": "https://www.ecosia.org/search?q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "MetaGer",
+      "iconUrl": "https://api.faviconkit.com/metager.de/64",
+      "searchUrl": "https://metager.de/meta/meta.ger3?eingabe={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "DuckDuckGo",
+      "iconUrl": "https://api.faviconkit.com/duckduckgo.com/64",
+      "searchUrl": "https://duckduckgo.com/?q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Google",
+      "iconUrl": "https://api.faviconkit.com/www.google.com/64",
+      "searchUrl": "https://www.google.com/search?q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "custom",
+      "name": "SLUB Dresden",
+      "searchUrl": "https://katalogbeta.slub-dresden.de/?tx_find_find[q][default]={searchTerms}",
+      "iconUrl": "https://api.faviconkit.com/katalogbeta.slub-dresden.de/64",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Google Scholar",
+      "iconUrl": "https://api.faviconkit.com/scholar.google.com/64",
+      "searchUrl": "https://scholar.google.com/scholar?q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Ecosia Images",
+      "iconUrl": "https://api.faviconkit.com/www.ecosia.org/64",
+      "searchUrl": "https://www.ecosia.org/images?q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Google Images",
+      "iconUrl": "https://api.faviconkit.com/www.google.com/64",
+      "searchUrl": "http://www.google.com/search?tbm=isch&tbs=imgo:1&safe=off&q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Twitter",
+      "iconUrl": "https://api.faviconkit.com/twitter.com/64",
+      "searchUrl": "https://twitter.com/search/realtime?q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Google News",
+      "iconUrl": "https://api.faviconkit.com/news.google.com/64",
+      "searchUrl": "https://news.google.com/search?q={searchTerms}&hl=en-SG&gl=SG&ceid=SG%3Aen",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Ngram Viewer DE",
+      "iconUrl": "https://api.faviconkit.com/books.google.com/64",
+      "searchUrl": "http://books.google.com/ngrams/graph?content={searchTerms}&year_start=1800&year_end=2012&corpus=20",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Wikipedia (Deutsch)",
+      "iconUrl": "https://api.faviconkit.com/de.wikipedia.org/64",
+      "searchUrl": "https://de.wikipedia.org/w/index.php?title=Spezial:Suche&search={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Wikimedia Commons",
+      "iconUrl": "https://api.faviconkit.com/commons.wikimedia.org/64",
+      "searchUrl": "http://commons.wikimedia.org/w/wiki.phtml?search={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "HERE WeGo",
+      "iconUrl": "https://api.faviconkit.com/wego.here.com/64",
+      "searchUrl": "https://wego.here.com/search/{searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "custom",
+      "name": "Google Maps",
+      "searchUrl": "https://www.google.de/maps/search/{searchTerms}",
+      "iconUrl": "https://api.faviconkit.com/maps.google.com/64",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "LEO de<->en",
+      "iconUrl": "https://api.faviconkit.com/dict.leo.org/64",
+      "searchUrl": "http://dict.leo.org/?lp=ende&lang=de&searchLoc=0&cmpType=relaxed&relink=on&sectHdr=on&spellToler=std&search={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "dict.cc",
+      "iconUrl": "https://api.faviconkit.com/www.dict.cc/64",
+      "searchUrl": "http://www.dict.cc/?s={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "YouTube",
+      "iconUrl": "https://api.faviconkit.com/www.youtube.com/64",
+      "searchUrl": "https://www.youtube.com/results?search_query={searchTerms}&search=Search",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "SoundCloud",
+      "iconUrl": "https://api.faviconkit.com/soundcloud.com/64",
+      "searchUrl": "https://soundcloud.com/search?q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "heise Preisvergleich",
+      "iconUrl": "https://api.faviconkit.com/www.heise.de/64",
+      "searchUrl": "https://www.heise.de/preisvergleich/?fs={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Amazon DE",
+      "iconUrl": "https://api.faviconkit.com/www.amazon.de/64",
+      "searchUrl": "https://www.amazon.de/s?k={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "eBay Deutschland (Sofort-Kaufen, Weltweit)",
+      "iconUrl": "https://api.faviconkit.com/www.ebay.de/64",
+      "searchUrl": "http://www.ebay.de/sch/i.html?_odkw=dfgh&LH_BIN=1&_sop=15&_osacat=0&clk_rvr_id=618702717996&LH_PrefLoc=2&_mPrRngCbx=1&_trksid=p2045573.m570.l1313.TR2.TRC0.A0.H0.XFUCKER&_nkw={searchTerms}&_sacat=0&_from=R40",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "eBay Kleinanzeigen",
+      "iconUrl": "https://www.ebay-kleinanzeigen.de/static/img/favicon.1hf6kaeaeu89h.png",
+      "searchUrl": "https://www.ebay-kleinanzeigen.de/s-{searchTerms}/k0",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "AliExpress.com",
+      "iconUrl": "https://api.faviconkit.com/www.aliexpress.com/64",
+      "searchUrl": "http://www.aliexpress.com/wholesale?SearchText={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Github",
+      "iconUrl": "https://api.faviconkit.com/github.com/64",
+      "searchUrl": "http://github.com/search?q={searchTerms}&type=Everything&repo=&langOverride=&start_value=1",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Chefkoch.de Rezeptsuche - Sortiert nach Wertung",
+      "iconUrl": "https://api.faviconkit.com/www.chefkoch.de/64",
+      "searchUrl": "http://www.chefkoch.de/rs/s0o8/{searchTerms}/Rezepte.html",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "IMDb",
+      "iconUrl": "https://api.faviconkit.com/www.imdb.com/64",
+      "searchUrl": "https://www.imdb.com/find?s=all&q={searchTerms}",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    },
+    {
+      "type": "browser",
+      "name": "Google Play - Apps",
+      "iconUrl": "https://api.faviconkit.com/play.google.com/64",
+      "searchUrl": "https://play.google.com/store/search?q={searchTerms}&c=apps",
+      "isEnabled": true,
+      "isEnabledInContextMenu": true
+    }
+  ],
+  "sectionsExpansionState": {
+    "section_searchEngines": true,
+    "section_reset": true,
+    "section_storageAndBackup": true,
+    "section_contextMenu": true,
+    "section_popupAppearance": true,
+    "section_popupBehaviour": true
+  },
+  "useSingleRow": false
+}
