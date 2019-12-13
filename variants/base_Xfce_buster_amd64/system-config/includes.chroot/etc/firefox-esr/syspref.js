a320552c50299640e6f82e8dc9156d6e067b5685
Marcel Partap <mpartap@gmx.net>
2019-12-13 22:24:47 +0100

    Xfce_buster firefox settings: disable more noise & distractions

--- variants/base_Xfce_buster_amd64/system-config/includes.chroot/etc/firefox-esr/syspref.js
+++ variants/base_Xfce_buster_amd64/system-config/includes.chroot/etc/firefox-esr/syspref.js
@@ -3,5 +3,11 @@
 // less window decoration = more space for content
 pref("browser.tabs.drawInTitlebar", true);
 
-// tab mouse wheel scrolling
+// enable tab mouse wheel scrolling
 pref("toolkit.tabbox.switchByScrolling", true);
+
+// disable slow visual tab switching
+pref("browser.ctrlTab.recentlyUsedOrder", false);
+
+// do not show addon recommendations
+pref("extensions.htmlaboutaddons.recommendations.enabled", false);
