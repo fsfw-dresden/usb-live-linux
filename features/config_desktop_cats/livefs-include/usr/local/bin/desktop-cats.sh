71808089b655aaf3239596a3ef0d60312b2f1073
Marcel Partap <mpartap@gmx.net>
2020-04-15 21:35:27 +0200

    hone features/config_desktop_cats start script

--- features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
+++ features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-SEED=$(( RANDOM % 10 ))
-[ ${SEED} -gt 6 ] && timeout $(( RANDOM % 9 + 1 ))m oneko -time 90000 -tora -idle 50 &
-[ ${SEED} -gt 8 ] && timeout $(( RANDOM % 14 + 1))m oneko -bg black -fg white &
+# wait a few seconds, then show a fast tiger cat 30% of the time for up to 9min
+{ sleep $(( RANDOM % 30 + 5 )) && [ $(( RANDOM % 10 + 1 )) -le 3 ] && timeout $(( RANDOM % 9 + 1 ))m oneko -time 90000 -tora -idle 50; } &
 
+# wait a few seconds, then show a dark cat 20% of the time for up to 14min
+{ sleep $(( RANDOM % 30 + 5 )) && [ $(( RANDOM % 10 + 1 )) -le 2 ] && timeout $(( RANDOM % 14 + 1))m oneko -bg black -fg white; } &
