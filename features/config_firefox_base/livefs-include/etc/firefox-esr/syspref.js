bf3623a3ba112aca95491e7715efe6691d7f94c4
Marcel Partap <mpartap@gmx.net>
2020-08-24 09:31:51 +0200

    Old-schoolize Firefox base settings

--- features/config_firefox_base/livefs-include/etc/firefox-esr/syspref.js
+++ features/config_firefox_base/livefs-include/etc/firefox-esr/syspref.js
@@ -1,13 +1,33 @@
 // This file can be used to configure global preferences for Firefox
-        
-// less window decoration = more space for content
+
+// Less window decoration = more space for content
 pref("browser.tabs.drawInTitlebar", true);
 
-// enable tab mouse wheel scrolling
+// Enable tab mouse wheel scrolling
 pref("toolkit.tabbox.switchByScrolling", true);
 
-// disable slow visual tab switching
+// Disable slow visual tab switching
 pref("browser.ctrlTab.recentlyUsedOrder", false);
 
-// do not show addon recommendations
+// Do not show addon recommendations
 pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+
+// Disable all web-notification garbage
+pref("dom.webnotifications.enabled", false);
+pref("dom.webnotifications.serviceworker.enabled", false);
+
+// Because one has to select the correct language for each text input
+// box to not see red, disable spell checking by default
+pref("layout.spellcheckDefault", 0);
+
+// Highlight all search matches
+pref("findbar.highlightAll", true);
+
+// Require pressing ENTER to execute search
+pref("accessibility.typeaheadfind", false);
+
+// Disable "open with" for files
+pref("browser.download.forbid_open_with", true);
+
+// Do not hide the bloody download button
+pref("browser.download.autohideButton", false);
