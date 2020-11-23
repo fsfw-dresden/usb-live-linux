19e40c39d60155a6fc36f487e58f14f29abb1494
Marcel Partap <mpartap@gmx.net>
2020-11-23 14:14:59 +0100

    Increase cat spread

--- features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
+++ features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# wait a few seconds, then show a fast tiger cat 15% of the time for up to 9min
-{ sleep $(( RANDOM % 30 + 5 )) && [ $(( RANDOM % 100 + 1 )) -le 15 ] && timeout $(( RANDOM % 9 + 1 ))m oneko -time 90000 -tora -idle 50 -cursor 132; } &
+# wait a few minutes, then show a fast tiger cat 15% of the time for up to 9min
+{
+    sleep $(( RANDOM % 30 * 60 ))
+    [ $(( RANDOM % 100 + 1 )) -le 15 ] && timeout $(( RANDOM % 9 + 1 ))m oneko -time 90000 -tora -idle 50 -cursor 132;
+} &
 
-# wait a few seconds, then show a dark cat 10% of the time for up to 14min
-{ sleep $(( RANDOM % 30 + 5 )) && [ $(( RANDOM % 100 + 1 )) -le 10 ] && timeout $(( RANDOM % 14 + 1))m oneko -bg black -fg white -cursor 132; } &
+# wait a few minutes, then show a dark cat 10% of the time for up to 14min
+{
+    sleep $(( RANDOM % 30 * 60 ))
+    [ $(( RANDOM % 100 + 1 )) -le 10 ] && timeout $(( RANDOM % 14 + 1))m oneko -bg black -fg white -cursor 132;
+} &
