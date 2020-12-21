2772f2ae6f6bdcaf7cf34fdd1a6b0de8153921ac
Marcel Partap <mpartap@gmx.net>
2020-12-21 17:54:15 +0100

    config_firefox_search_schulstick: Make 🌳Ecosia search default: TREES++
    - it uses the same Microsoft Bing search results as DuckDuckGo
    - disable ad-blocking on Ecosia as ads pay for the TREES 💚🌳🌳🌲🎄🌳

---
+++ features/config_firefox_extensions/user-config/.mozilla/firefox/fsfw1234.default/browser-extension-data/uBlock0@raymondhill.net/storage.js
@@ -0,0 +1,13 @@
+{
+  "netWhitelist": [
+    "about-scheme",
+    "chrome-extension-scheme",
+    "chrome-scheme",
+    "edge-scheme",
+    "moz-extension-scheme",
+    "opera-scheme",
+    "vivaldi-scheme",
+    "www.ecosia.org",
+    "wyciwyg-scheme"
+  ]
+}
