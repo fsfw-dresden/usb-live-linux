666e542148349217f1aee09b55b021d3b56141a7
Marcel Partap <mpartap@gmx.net>
2021-11-15 23:42:54 +0100

    config_desktop_cats: reduce random cat presence to bearable times

--- features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
+++ features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# wait a few minutes, then show a fast tiger cat 15% of the time for up to 9min
-{
-    sleep $(( RANDOM % 30 * 60 ))
-    [ $(( RANDOM % 100 + 1 )) -le 15 ] && timeout $(( RANDOM % 9 + 1 ))m oneko -time 90000 -tora -idle 50 -cursor 132;
-} &
-
-# wait a few minutes, then show a dark cat 10% of the time for up to 14min
-{
-    sleep $(( RANDOM % 30 * 60 ))
-    [ $(( RANDOM % 100 + 1 )) -le 10 ] && timeout $(( RANDOM % 14 + 1))m oneko -bg black -fg white -cursor 132;
-} &
+# show a fast tiger cat 15% of the time for up to 90sec
+#         or a dark cat 10% of the time for up to 60sec
+# and wait up to 30 minutes before doing it
+[ $(( RANDOM % 100 + 1 )) -le 15 ] && sleep $(( RANDOM % 30 * 60 )) && timeout $(( RANDOM % 100 + 1 ))s oneko -time 90000 -tora -idle 50 -cursor 132 &
+[ $(( RANDOM % 100 + 1 )) -le 10 ] && sleep $(( RANDOM % 30 * 60 )) && timeout $(( RANDOM % 60 + 1))s oneko -bg black -fg white -cursor 132  &
