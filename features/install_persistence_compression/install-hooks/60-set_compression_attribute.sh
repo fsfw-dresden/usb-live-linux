70fea6d92744483ca55d777ad4e57e507181c38b
Marcel Partap <mpartap@gmx.net>
2022-02-06 15:32:29 +0100

    Deslag persistence setup scripts (FIXME: code duplication, compression?)

--- features/install_persistence_compression/install-hooks/60-set_compression_attribute.sh
+++ features/install_persistence_compression/install-hooks/60-set_compression_attribute.sh
@@ -1,6 +1,7 @@
 if grep " ${PERSISTENCESTORE} f2fs" /proc/mounts
 then
     print_info "Setting compression attribute on persistence store"
+    print_warn "(FIXME: does not seem to be effective?)"
     find ${PERSISTENCESTORE} -type d -exec chattr -V +c {} +
 else
     print_warn "partition ${PERSISTENCESTORE} seems not to be mounted, skipping.."
