b10c6f6aeb0924d94adce7a2cf4a108d29fe65eb
Marcel Partap <mpartap@gmx.net>
2022-02-06 17:32:57 +0100

    install_live_system_iso: copy system ISO to root of 2nd partition
    .. instead of placing it into /boot subdirectory

--- features/install_live_system_iso/install-hooks/10-copy_live_system_iso.sh
+++ features/install_live_system_iso/install-hooks/10-copy_live_system_iso.sh
@@ -1,8 +1,7 @@
-mkdir -pv ${ISOSTORE}/boot
-print_info "now copying $(numfmt --to=iec-i --suffix B ${SIZE_LIVE_SYSTEM}) live ISO image to ${DEVICE}${p}2:/boot"
+print_info "now copying $(numfmt --to=iec-i --suffix B ${SIZE_LIVE_SYSTEM}) live ISO image to ${DEVICE}${p}2"
 print_warn "(Note that, because of write caching, the file transfer speed displayed may only represent actual device write speed after write buffer has filled.)"
 
-rsync --times --inplace --info=progress2 --human-readable --copy-links "../${LIVE_IMAGE}" ${ISOSTORE}/boot/ \
+rsync --times --inplace --info=progress2 --human-readable --copy-links "../${LIVE_IMAGE}" ${ISOSTORE}/ \
     && print_info "synchronizing unwritten data to disk.." \
-    && sync ${ISOSTORE}/boot/ \
+    && sync ${ISOSTORE}/ \
     && print_success "copying ${LIVE_IMAGE} to ${DEVICE} completed; please wait for all scripts to finish"
