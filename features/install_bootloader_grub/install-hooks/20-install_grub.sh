9897c5597e52b3d3a50ab7dde2b37a637ab2d12a
Marcel Partap <mpartap@gmx.net>
2022-03-31 01:09:32 +0200

    Fix running stick-install inside booted live system & remove cruft code
    - remove support for github actions workaround and ubuntu shenanigans

--- features/install_bootloader_grub/install-hooks/20-install_grub.sh
+++ features/install_bootloader_grub/install-hooks/20-install_grub.sh
@@ -1,16 +1,14 @@
 check_dependencies grub-pc-bin grub-efi-ia32-bin shim-signed syslinux-common libcdio-utils
 
-# FIXME: remove this? had been used to debug failing github-actions build..
-# if the image is small, it is a test image to short cut an automated build process
-if [ ${SIZE_LIVE_SYSTEM} -gt 16777216 ]
-then
+(
+    # LIVE_IMAGE URI is either absolute or relative to repo root, so cd up
+    cd ..
+
     mkdir -p ${EFIBOOT}/boot
     print_info "extracting kernel and init ramdisk from ISO to directly boot partition of type 0"
-    iso-read -e live/vmlinuz -o ${EFIBOOT}/boot/vmlinuz -i ../${LIVE_IMAGE}
-    iso-read -e live/initrd.img -o ${EFIBOOT}/boot/initrd.img -i ../${LIVE_IMAGE}
-else
-    print_warn "skipping kernel/initrd extraction for fake build test ISO: this will not boot!"
-fi
+    iso-read -e live/vmlinuz -o ${EFIBOOT}/boot/vmlinuz -i ${LIVE_IMAGE}
+    iso-read -e live/initrd.img -o ${EFIBOOT}/boot/initrd.img -i ${LIVE_IMAGE}
+)
 
 GRUB_VERSION=$(dpkg-query --show --showformat='${Version}' grub2-common)
 print_info "installing grub bootloader for i386-pc, i386-efi and x86_64-efi platform to ${DEVICE}\n\t(should take 5-20 seconds each)"
@@ -19,13 +17,4 @@ print_info "installed grub version seems to be ${GRUB_VERSION} .."
 # --uefi-secure-boot is default in 2.02, but make it explicit
 grub-install --target=i386-efi --uefi-secure-boot --no-nvram --recheck --removable --efi-directory=${EFIBOOT} --root-directory=${EFIBOOT}
 grub-install --target=i386-pc --no-floppy --force --removable --root-directory=${EFIBOOT} ${DEVICE}
-
-# ubuntu is missing https://salsa.debian.org/grub-team/grub/-/blob/master/debian/patches/grub-install-extra-removable.patch
-dpkg-query --show grub2-common | grep -qs "ubuntu" && FORCE_EXTRA= || FORCE_EXTRA="--force-extra-removable"
-
-grub-install --target=x86_64-efi --uefi-secure-boot --no-nvram ${FORCE_EXTRA} --efi-directory=${EFIBOOT} --root-directory=${EFIBOOT}
-
-# FIXME: remove? will we ever use the github actions build again?
-# something is twisted wrong with the github build VM:
-# with all grub packages from debian pulled in, it still misnames the EFI folder
-[ ! -d ${EFIBOOT}/EFI/ubuntu ] || mv -v ${EFIBOOT}/EFI/ubuntu ${EFIBOOT}/EFI/debian
+grub-install --target=x86_64-efi --uefi-secure-boot --no-nvram --force-extra-removable --efi-directory=${EFIBOOT} --root-directory=${EFIBOOT}
--- features/install_live_system_iso/install-hooks/10-copy_live_system_iso.sh
+++ features/install_live_system_iso/install-hooks/10-copy_live_system_iso.sh
@@ -1,7 +1,12 @@
 print_info "now copying $(numfmt --to=iec-i --suffix B ${SIZE_LIVE_SYSTEM}) live ISO image to ${DEVICE}${p}2"
 print_warn "(Note that, because of write caching, the file transfer speed displayed may only represent actual device write speed after write buffer has filled.)"
 
-rsync --times --inplace --info=progress2 --human-readable --copy-links "../${LIVE_IMAGE}" ${ISOSTORE}/ \
-    && print_info "synchronizing unwritten data to disk.." \
-    && sync ${ISOSTORE}/ \
-    && print_success "copying ${LIVE_IMAGE} to ${DEVICE} completed; please wait for all scripts to finish"
+(
+    # LIVE_IMAGE URI is either absolute or relative to repo root, so cd up
+    cd ..
+
+    rsync --times --inplace --info=progress2 --human-readable --copy-links "${LIVE_IMAGE}" ${ISOSTORE}/ \
+        && print_info "synchronizing unwritten data to disk.." \
+        && sync ${ISOSTORE}/ \
+        && print_success "copying ${LIVE_IMAGE} to ${DEVICE} completed; please wait for all scripts to finish"
+)
