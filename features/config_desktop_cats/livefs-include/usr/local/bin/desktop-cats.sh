3815c2854a4666975897396054dd273bffb9c714
Marcel Partap <mpartap@gmx.net>
2023-02-21 16:45:59 +0100

    config_desktop_cats: fix restoring normal mouse cursor
    Normal X11 cursor is 68 (left_ptr), not 132 (top_left_arrow)!

--- features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
+++ features/config_desktop_cats/livefs-include/usr/local/bin/desktop-cats.sh
@@ -2,5 +2,5 @@
 # show a fast tiger cat 5% of the time for up to 90sec
 #         or a dark cat 5% of the time for up to 60sec
 # and wait up to 30 minutes before doing it
-[ $(( RANDOM % 100 + 1 )) -le 5 ] && sleep $(( RANDOM % 30 * 60 )) && timeout $(( RANDOM % 100 + 1 ))s oneko -time 90000 -tora -idle 50 -cursor 132 &
-[ $(( RANDOM % 100 + 1 )) -le 5 ] && sleep $(( RANDOM % 30 * 60 )) && timeout $(( RANDOM % 60 + 1))s oneko -bg black -fg white -cursor 132  &
+[ $(( RANDOM % 100 + 1 )) -le 5 ] && sleep $(( RANDOM % 30 * 60 )) && timeout $(( RANDOM % 100 + 1 ))s oneko -time 90000 -tora -idle 50 -cursor 68 &
+[ $(( RANDOM % 100 + 1 )) -le 5 ] && sleep $(( RANDOM % 30 * 60 )) && timeout $(( RANDOM % 60 + 1))s oneko -bg black -fg white -cursor 68  &
