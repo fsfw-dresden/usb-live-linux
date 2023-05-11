87fb22e95513e9e02f60f5d19d4efa262df66322
Marcel Partap <mpartap@gmx.net>
2023-05-11 03:28:17 +0200

    install_bootmenu_german: warn instead of fail when installing offline

--- features/install_bootmenu_german/install-hooks/30-download-advanced-tools.sh
+++ features/install_bootmenu_german/install-hooks/30-download-advanced-tools.sh
@@ -5,9 +5,13 @@
 URL_HDT_ISO=https://github.com/knightmare2600/hdt/raw/master/hdt-0.5.2.iso
 FILE=$(download_file_cached ${URL_HDT_ISO}) \
     && cp -a ${FILE} ${EFIBOOT}/boot/ \
-    && touch --date="2012-09-10 21:03:50 +0200" ${EFIBOOT}/boot/${FILE##*/}
+    && touch --date="2012-09-10 21:03:50 +0200" ${EFIBOOT}/boot/${FILE##*/} \
+    || print_warn "Could not download optional Hardware Detection Tool, please check" \
+       " whether a network connection\n\tis active and the URL ${URL_HDT_ISO} is working"
 
 URL_MEMTEST=https://memtest.org/download/v6.10/mt86plus_6.10_64.iso.zip
 FILE=$(download_file_cached ${URL_MEMTEST}) \
     && unzip -d ${EFIBOOT}/boot/ ${FILE} \
-    && mv ${EFIBOOT}/boot/$(basename --suffix=.zip ${FILE}) ${EFIBOOT}/boot/mt86plus.iso
+    && mv ${EFIBOOT}/boot/$(basename --suffix=.zip ${FILE}) ${EFIBOOT}/boot/mt86plus.iso \
+    || print_warn "Could not download optional Memtest86+, please check" \
+       " whether a network connection\n\tis active and the URL ${URL_MEMTEST} is working"
