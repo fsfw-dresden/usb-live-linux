484092e66cca0dcc6c389e8757a1980354eb942d
Marcel Partap <mpartap@gmx.net>
2023-09-17 16:18:36 +0200

    Minor fixes on various bash scripts

--- hotfixes/Gymnasium-Dresden-Plauen-Schulstick_2535b__bookworm__bookworm_bookworm-amd64/install-hooks/copy_to_persistence.sh
+++ hotfixes/Gymnasium-Dresden-Plauen-Schulstick_2535b__bookworm__bookworm_bookworm-amd64/install-hooks/copy_to_persistence.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+# abort on error
+set -e
 
 PERSISTENCESTORE=$(mktemp --tmpdir --directory hotfix.XXXX)
-mount -v ${1}3 ${PERSISTENCESTORE}
+mount -v ${1}3 ${PERSISTENCESTORE} && trap "umount -v ${PERSISTENCESTORE} && rmdir ${PERSISTENCESTORE}" EXIT SIGHUP SIGQUIT SIGTERM
 cp -av install-data/* ${PERSISTENCESTORE}/
-umount -v ${PERSISTENCESTORE}
-rmdir ${PERSISTENCESTORE}
