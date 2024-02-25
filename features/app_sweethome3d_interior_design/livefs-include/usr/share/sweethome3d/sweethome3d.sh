aa24380a9a6510abd54a636e59f3791afc0ab0ec
Marcel Partap <mpartap@gmx.net>
2024-02-25 22:59:06 +0100

    app_sweethome3d_interior_design: drop ancient workarounds so less glitch

--- features/app_sweethome3d_interior_design/livefs-include/usr/share/sweethome3d/sweethome3d.sh
+++ features/app_sweethome3d_interior_design/livefs-include/usr/share/sweethome3d/sweethome3d.sh
@@ -6,8 +6,16 @@ JAVA_ARGS="-Djava.library.path=/usr/lib/jni \
  -Dcom.eteks.sweethome3d.applicationFolders=$HOME/.eteks/sweethome3d:/usr/share/sweethome3d \
  --add-exports java.desktop/sun.awt=ALL-UNNAMED"
 
-! systemd-detect-virt -qv && JAVA_ARGS="${JAVA_ARGS} -Dsun.java2d.opengl=true \
- -Dcom.eteks.sweethome3d.j3d.checkOffScreenSupport=false"
+# These have aged poorly and cause glitches,
+# so both of these parameters removed:
+# 387805e5c19cba3ee0691f991a45d9b1e94fac84i (2017-03-05)
+#   Add -Dsun.java2d.opengl=true to startup script.
+#   Closes: #854030
+# 2793f4a3b23bcca09f13cc8557c201cbfc6d7599  (2016-10-12)
+#  * Set -Dcom.eteks.sweethome3d.j3d.checkOffScreenSupport=false to fix crashes
+#    and segmentation faults when trying to take a photo or open the preferences
+#    menu. Thanks to Török Edwin for the suggested fix.
+#    (Closes: #838387, #750821)
 
 . /usr/lib/java-wrappers/java-wrappers.sh
 
