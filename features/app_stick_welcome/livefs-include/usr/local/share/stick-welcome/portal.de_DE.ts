7b9958d038e613ae2f582dffa83c8d1f6e85cc74
Marcel Partap <mpartap@gmx.net>
2024-08-17 22:27:11 +0200

    app_stick_welcome: add translation stub and background image
    and a few minor source changes

---
+++ features/app_stick_welcome/livefs-include/usr/local/share/stick-welcome/portal.de_DE.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>StickPortal</name>
+    <message>
+        <location filename="portal.ui" line="14"/>
+        <source>Stick Portal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="33"/>
+        <source>Welcome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="45"/>
+        <source>Welcome to this first mockup of a FSFW-Stick-Portal app.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="58"/>
+        <source>Copy Live System to USB-Stick or hard disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="71"/>
+        <source>Install non-free but popular applications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="84"/>
+        <source>Set password and encryption</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="97"/>
+        <source>INTRO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="128"/>
+        <source>Tutorials</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="147"/>
+        <source>https://www.youtube.com/embed/d0tU18Ybcvk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="161"/>
+        <source>Placeholder: a stream of tutorials shall be displayed here.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="167"/>
+        <source>Features</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="173"/>
+        <source>Features / Variants guide (TODO: screenshots, links, … )</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="183"/>
+        <source>https://raw.githack.com/fsfw-dresden/usb-live-linux/master/variants.build/features.html</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="195"/>
+        <source>News</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="207"/>
+        <source>QWebView widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="238"/>
+        <source>https://github.com/fsfw-dresden/usb-live-linux/blob/master/variants.build/FSFW-Schulstick/README.md</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="231"/>
+        <source>QWebEngineView widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="250"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="256"/>
+        <source>Duplicate to USB pen drive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="271"/>
+        <source>Support</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="283"/>
+        <source>Post your Question here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="296"/>
+        <source>If you need help resolving your issue, an FSFW technician might be available to remotely fix it / show what to do</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="330"/>
+        <source>Feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="336"/>
+        <source>How do you rate this FSFW Live Stick as a tool?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="352"/>
+        <source>Send Feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="263"/>
+        <source>Launch Installer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="308"/>
+        <source>Start Rustdesk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="portal.ui" line="321"/>
+        <source>Then send us your rustdesk number via above query form.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
