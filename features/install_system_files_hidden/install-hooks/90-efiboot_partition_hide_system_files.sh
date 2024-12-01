721a225189d40f64c41b9ed64e03b6c068d67919
Marcel Partap <mpartap@gmx.net>
2024-12-01 21:00:31 +0100

    Fix install_system_files_hidden script

--- features/install_system_files_hidden/install-hooks/90-efiboot_partition_hide_system_files.sh
+++ features/install_system_files_hidden/install-hooks/90-efiboot_partition_hide_system_files.sh
@@ -1,13 +1,17 @@
 check_dependencies fatattr
 
 # precreate qemu EFI bios file so it can be hidden in next step
-touch ${EFIBOOT}/NvVars
+touch "${EFIBOOT}/NvVars"
+
+HIDEFILE="${EFIBOOT}/.hidden"
 
 print_info "marking files on boot / exchange as hidden system files\n" \
     "\tto make accidental deletion in Windows less likely" \
     "\t(also, hide these files in linux, too: via .hidden)"
 for FILE in "boot" "EFI" "NvVars" "System Volume Information"
 do
-  fatattr +hs "${EFIBOOT}/${FILE}" ${EFIBOOT}/.hidden
-  echo "${FILE}" >> ${EFIBOOT}/.hidden
+  FILEPATH="${EFIBOOT}/${FILE}"
+  [ -e "${FILEPATH}" ] && fatattr +hs "${FILEPATH}"
+  echo "${FILE}" >> "${HIDEFILE}"
 done
+fatattr +hs "${HIDEFILE}"
