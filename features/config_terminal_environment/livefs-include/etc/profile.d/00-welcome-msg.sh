3cfc3a8f304a5c258e623193ddf3087a39766334
Marcel Partap <mpartap@gmx.net>
2025-11-11 09:04:16 +0100

    config_terminal_environment: don't show welcome message in IDEs/VTs
    (also, some minor polishing)

--- features/config_terminal_environment/livefs-include/etc/profile.d/00-welcome-msg.sh
+++ features/config_terminal_environment/livefs-include/etc/profile.d/00-welcome-msg.sh
@@ -5,30 +5,34 @@
 # proceed only if shell interactivity flag is set
 case "$-" in
   *i* )
-    # proceed when not in ranger or tmux, non-root user
-    [ -n "$RANGER_LEVEL" -o -n "$TMUX" -o $(id -u) -eq 0 ] && return ;;
+    # proceed when not in ranger or tmux, non-root user, not a linux VT
+    # - and do not mess with invoked environments (IDEs, …)
+    [ -n "$RANGER_LEVEL" ] || [ -n "$TMUX" ] || [ "$(id -u)" -eq 0 ] || \
+      tty | grep -qs ^/dev/tty \\
+      grep -qE -- "--init-file|--rcfile|--norc|--noprofile" /proc/$$/cmdline && \
+      return ;;
   * ) return ;;
 esac
 
 COLOR_CYAN='\033[1;36m'
 COLOR_OFF='\033[0;0m'
 
-# needs lolcat and chafa or toilet
-[ -x /usr/games/lolcat ] && ( [ -x /usr/bin/chafa ] || [ -x /usr/bin/toilet ] ) || return
+# needs lolcat and chafa and toilet
+[ -x /usr/games/lolcat ] && [ -x /usr/bin/chafa ] && [ -x /usr/bin/toilet ] || return
 
-if [ $LANG = "de_DE.UTF-8" ]
+if [ "$LANG" = "de_DE.UTF-8" ]
 then
   echo "    Willkommen zum Kommandozeilenmodus. Computer wartet auf deine Befehle." | lolcat
-  echo -e "	( Tasten ${COLOR_CYAN}[⬆️ UMSCHALT]+[F1]${COLOR_OFF} zum Umschalten hier zum Terminal und zurück )"
+  printf "	( Tasten ${COLOR_CYAN}[⬆️ UMSCHALT]+[F1]${COLOR_OFF} zum Umschalten hier zum Terminal und zurück )\n"
   [ -f /usr/local/share/icons/keine-panik.svg ] \
-    && chafa --colors 16 --size $(tput cols)x18 --color-space din99d --fill braille --work 2 --threshold 1 -- /usr/local/share/icons/keine-panik.svg | lolcat \
-    || echo "KEINE PANIK"|toilet --filter border | lolcat
+    && chafa --format symbols --colors 16 --size $(tput cols)x18 --color-space din99d --fill braille --work 2 --threshold 1 -- /usr/local/share/icons/keine-panik.svg | lolcat \
+    || echo "KEINE PANIK" | toilet --filter border | lolcat
 else
   echo "    Welcome to the command line interface. Computer is waiting for your input." | lolcat
-  echo -e "	( Press ${COLOR_CYAN}[⬆️ SHIFT]+[F1]${COLOR_OFF} keys to show and hide this terminal )"
+  printf "	( Press ${COLOR_CYAN}[⬆️ SHIFT]+[F1]${COLOR_OFF} keys to show and hide this terminal )\n"
   [ -f /usr/local/share/icons/dont-panic.svg ] \
-    && chafa --colors 16 --size $(tput cols)x18 --color-space din99d --fill braille --work 2 --threshold 1 -- /usr/local/share/icons/dont-panic.svg | lolcat \
-    || echo "don't panic"|toilet --filter border | lolcat
+    && chafa --format symbols --colors 16 --size $(tput cols)x18 --color-space din99d --fill braille --work 2 --threshold 1 -- /usr/local/share/icons/dont-panic.svg | lolcat \
+    || echo "don't panic" | toilet --filter border | lolcat
 fi
 
 echo
